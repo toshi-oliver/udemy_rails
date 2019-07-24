5bcd6522dba4e6facdece48c544c73859d24d0e7
toshi-oliver <igpoid@gmail.com>
2019-07-24 16:48:22 +0900

    modified boards_controller.rb

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -7,5 +7,12 @@ class BoardsController < ApplicationController
   end
 
   def create
+    Board.create(board_params)
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name, :title, :body)
   end
 end
