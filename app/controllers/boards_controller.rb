589577067eebe4c5e1c829453483e757f918541d
toshi-oliver <igpoid@gmail.com>
2019-07-29 00:42:51 +0900

    add flash message to update_action

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,7 +6,7 @@ class BoardsController < ApplicationController
   end
 
   def new
-    @@board = Board.new(flash[:board])
+    @board = Board.new(flash[:board])
   end
 
   def create
@@ -15,29 +15,32 @@ class BoardsController < ApplicationController
       flash[:notice] = "「#{board.title}」の掲示板を作成しました"
       redirect_to board
     else
-      redirect_to :back, flash: {
-        board: board,
-        error_messages: board.errors.full_messages
-      }
+      flash[:board] = board
+      flash[:error_messages] = board.errors.full_messages
+      redirect_back fallback_location: board
     end
   end
 
   def show
     @comment = Comment.new(board_id: @board.id)
-    #よくわからん
   end
 
   def edit
   end
 
   def update
-    @board.update(board_params)
-    redirect_to @board
+    if @board.update(board_params)
+      redirect_to @board
+    else
+      flash[:board] = @board
+      flash[:error_messages] = @board.errors.full_messages
+      redirect_back fallback_location: @board
+    end
   end
 
   def destroy
     @board.delete
-    redirect_to boards_path, flash: { notice: "「#{@board.title}」の掲示板が削除されました" }
+    redirect_to boards_path, flash: { notice: "「#{@board.title}」の掲示板が削除されました"}
   end
 
   private
